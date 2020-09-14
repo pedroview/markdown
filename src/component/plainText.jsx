c595dde67dc64c56c35019405b98029c9543f2f5
pedromass <maduekwepedro@gmail.com>
2020-09-14 20:54:49 +0100

    changed textfield textarea in plaintext.jsx

--- src/component/plainText.jsx
+++ src/component/plainText.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
+import TextAreaAutosize from "@material-ui/core/TextAreaAutosize";
+import { withStyles } from "@material-ui/core/styles";
 
-export default ({ userInput, setUserInput }) => {
+export default (props) => {
+  const height = window.innerHeight;
+  const labelOffset = -60;
+  const focused = true;
+
+  const { userInput, setUserInput, classes } = props;
   return (
     <Paper elevation={4} className="plainText">
-      <textarea className="input-text" onChange={({ target }) => setUserInput(target.value)} defaultValue={userInput} />
+      {/* <TextField variant="outlined" InputProps={{ classes: { input: classes.input } }} /> */}
+      <TextAreaAutosize
+        placeholder="dfdf"
+        // fullWidth
+        // label="Example"
+        // variant="outlined"
+        // style={{ height, marginTop: "10px" }}
+        // InputLabelProps={{ style: { height, ...(!focused && { top: `${labelOffset}px` }) } }}
+        // inputProps={{ style: { height, padding: "0 14px", textAlign: "start" } }}
+      />
+      {/* <textarea className="input-text" onChange={({ target }) => setUserInput(target.value)} defaultValue={userInput} /> */}
     </Paper>
   );
 };
